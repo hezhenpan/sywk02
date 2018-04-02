f9fca1cf6b1fee11b4916c5d7fd5d16d52126658
hezhenpan <hezhenpan@163.com>
2018-04-02 19:17:41 +0800

    shy7

--- news/app.py
+++ news/app.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from pymongo import MongoClient
 
 
 
@@ -21,6 +22,8 @@ db = SQLAlchemy(app)
 engine = create_engine('mysql://root:@localhost/shiyanlou')
 Session = sessionmaker(bind=engine)
 session = Session()
+client = MongoClient('127.0.0.1',27017)
+mdb = client.shiyanlou
 
 
 
@@ -40,6 +43,31 @@ class File(db.Model):
         self.created_time = ctime
         self.category = category
         self.content = content
+    
+    def add_tag(self,tag_name):
+        tag = []
+        if self.tags:
+            
+            tag.append(tag_name)
+            mdb.file.insert_one({'title':self.title,'tags': tag})
+        else:
+            tag = self.tags
+            tag.append(tag_name)
+            mdb.file.update_one({'title':self.title}, {'$set': {'tags': tag}})
+
+    def remove_tag(self,tag_name):
+        self.tags.pop(tag_name)
+        mdb.file.update_one({'title':self.title}, {'$set': {'tags': self.tags}})
+
+    @property
+    def tags(self):
+        tmp = mdb.file.find_one({'title':self.title})
+        if tmp is None:
+            return []
+        else:
+            tags = tmp['tags']  
+            return tags
+
 
     def __repr__(self):
         return '<File %r>' % self.title
@@ -63,7 +91,10 @@ class Category(db.Model):
 @app.route('/')
 def index():
     flist = session.query(File).all()
-    return render_template('index.html',flist=flist)
+    tags = []
+    tags.append(mdb.file.find_one({'title':flist[0]}))
+    tags.append(mdb.file.find_one({'title':flist[1]}))
+    return render_template('index.html',flist=flist,tags=tags)
 
 
 
