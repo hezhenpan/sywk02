84b35064b223e6175c50306b10bb230709799e9c
hezhenpan <hezhenpan@hotmail.com>
2018-04-03 11:12:26 +0800

    Update app.py

--- news/app.py
+++ news/app.py
@@ -91,8 +91,21 @@ class Category(db.Model):
     def __repr__(self):
         return '<Category %r>' % self.name
 
-
-
+def insert_datas():
+    java = Category('Java')
+    python = Category('Python')
+    file1 = File('Hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
+    file2 = File('Hello Python', datetime.utcnow(), python, 'File Content - Python is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
+    file1.add_tag('tech')
+    file1.add_tag('java')
+    file1.add_tag('linux')
+    file2.add_tag('tech')
+    file2.add_tag('python')
 
 
 
